e41b0e1279209769a7a3090f1b13dc9e3732b509
Evan Roberts <eroberts1998@icloud.com>
2023-08-26 15:53:25 -0400

    shortened and minified backend code

--- api/index.py
+++ api/index.py
@@ -1,427 +1,128 @@
-from flask_migrate import Migrate
-from sqlalchemy import desc
-from flask import request
-from flask_restful import Resource
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from api.config import app, db, api
-from api.models import db, User, Recipe, Cocktail, OurPick, Favorite
-
-migrate = Migrate(app, db)
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.filter_by(id=user_id).first()
-
-# User Routes
-
-
-class Signup(Resource):
-
-    def post(self):
-        data = request.get_json()
-        new_user = User(
-            username=data['username'],
-            name=data['name'],
-            email=data['email'],
-        )
-        new_user.password_hash = data['password']
-
-        db.session.add(new_user)
-        db.session.commit()
-
-        login_user(new_user, remember=True)
-
-        return new_user.to_dict(), 201
-
-
-class Login(Resource):
-
-    def post(self):
-        try:
-            data = request.get_json()
-            identifier = data.get('identifier')  # Can be email or username
-            password = data.get('password')
-
-            user = User.query.filter(
-                (User.email == identifier) | (User.username == identifier)
-            ).first()
-
-            if user:
-                if user.authenticate(password):
-                    login_user(user, remember=True)
-                    return user.to_dict(), 200
-            if not user:
-                return {'error': '404 user not found'}, 404
-        except:
-            return {'error': '401 Unauthorized'}, 401
-
-
-@app.route("/logout", methods=["POST"])
-@login_required
-def logout():
-    logout_user()
-    return f'You have logged out. Goodbye'
-
-
-class CheckSession(Resource):
-
-    def get(self):
-        if current_user.is_authenticated:
-            user = current_user.to_dict()
-            return user, 200
-        return {"error": "unauthorized"}, 401
-
-
-# User Details Routes
-
-# class Users(Resource):
-#     @login_required
-#     def get(self):
-#         try:
-#             user = current_user
-#             if user:
-#                 return {
-#                     "id": user.id,
-#                     "name": user.name,
-#                     "email": user.email
-#                 }, 200
-#             else:
-#                 return {"error": "User not found"}, 404
-#         except:
-#             return {"error": "An error occurred while fetching the user"}, 500
-
-#     @login_required
-#     def patch(self):
-#         try:
-#             user = current_user
-#             if user:
-#                 data = request.get_json()
-#                 user.email = data.get("email", user.email)
-
-#                 # Hash the password before updating
-#                 password = data.get("password")
-#                 if password:
-#                     hashed_password = generate_password_hash(password)
-#                     user._password_hash = hashed_password
-
-#                 db.session.commit()
-#                 return {
-#                     "id": user.id,
-#                     "name": user.name,
-#                     "email": user.email
-#                 }, 200
-#             else:
-#                 return {"error": "User not found"}, 404
-#         except:
-#             return {"error": "An error occurred while updating the user"}, 500
-
-#     @login_required
-#     def delete(self):
-#         try:
-#             user = current_user
-#             if user:
-#                 db.session.delete(user)
-#                 db.session.commit()
-#                 return {}, 204
-#             else:
-#                 return {"error": "User not found"}, 404
-#         except:
-#             return {"error": "An error occurred while deleting the user"}, 500
-
-
-class UserFavorites(Resource):
-
-    @login_required
-    def get(self):
-        try:
-            user_id = current_user.id
-            favorites = Favorite.query.filter_by(user_id=user_id).all()
-            favorites_list = [favorite.to_dict() for favorite in favorites]
-            return favorites_list, 200
-        except Exception as e:
-            return {"error": "An error occurred while fetching favorites", "message": str(e)}, 500
-
-    @login_required
-    def post(self):
-        try:
-            favorite_data = request.get_json()
-
-            favorite_type = favorite_data.get('type')
-
-            if favorite_type == 'recipes':
-                new_favorite = Favorite(
-                    user_id=current_user.id,
-                    recipe_id=favorite_data['id']
-                )
-            elif favorite_type == 'cocktail':
-                new_favorite = Favorite(
-                    user_id=current_user.id,
-                    cocktail_id=favorite_data['id']
-                )
-            else:
-                return {"error": "Invalid favorite type"}, 400
-
-            db.session.add(new_favorite)
-            db.session.commit()
-
-            return "Added to favorites", 201
-
-        except Exception as e:
-            return {"error": "Failed to create favorite", "message": str(e)}, 500
-
-
-class UserFavoritesById(Resource):
-
-    @login_required
-    def get(self, id):
-        try:
-            favorite = Favorite.query.filter_by(id=id).first().to_dict()
-            return favorite, 200
-        except Exception as e:
-            return {"error": "An error occurred while fetching favorite", "message": str(e)}, 500
-
-    @login_required
-    def patch(self, id):
-        try:
-            data = request.get_json()
-            favorite = Favorite.query.filter_by(id=id).first()
-
-            if favorite:
-                for attr, value in data.items():
-                    setattr(favorite, attr, value)
-                db.session.commit()
-                return favorite.to_dict(), 200
-            else:
-                return {"error": "favorite not found"}, 404
-        except Exception as e:
-            return {"error": "Failed to update favorite", "message": str(e)}, 500
-
-    @login_required
-    def delete(self, id):
-        try:
-            favorite = Favorite.query.filter_by(id=id).first()
-            if favorite:
-                db.session.delete(favorite)
-                db.session.commit()
-                return {}, 204
-            else:
-                return {"error": "Favorite not found"}, 404
-        except Exception as e:
-            return {"error": "an error occurred while deleting the favorite"}, 500
-
-
-api.add_resource(Login, '/logins')
-api.add_resource(Signup, '/signups')
-api.add_resource(CheckSession, '/check_session')
-# api.add_resource(Users, '/users')
-api.add_resource(UserFavorites, '/users/favorites')
-api.add_resource(UserFavoritesById, '/users/favorites/<int:id>')
-
-# Recipe Routes
-
-
-class Recipes(Resource):
-
-    def get(self):
-        try:
-            recipes = [recipe.to_dict() for recipe in Recipe.query.all()]
-            return recipes, 200
-        except Exception as e:
-            return {"error": "Failed to retrieve recipes", "message": str(e)}, 500
-
-    def post(self):
-        try:
-            recipe_data = request.get_json()
-
-            new_recipe = Recipe(**recipe_data)
-
-            db.session.add(new_recipe)
-
-            db.session.commit()
-
-            return new_recipe.to_dict(), 201
-
-        except Exception as e:
-            return {"error": "Failed to create recipe", "message": str(e)}, 500
-
-
-api.add_resource(Recipes, '/recipe')
-
-
-class RecipesById(Resource):
-
-    def get(self, id):
-        try:
-            recipe = Recipe.query.filter_by(id=id).first().to_dict()
-            return recipe, 200
-        except Exception as e:
-            return {"error": "Failed to find recipe", "message": str(e)}, 500
-
-    def patch(self, id):
-        try:
-            data = request.get_json()
-            recipe = Recipe.query.filter_by(id=id).first()
-
-            if recipe:
-                for attr, value in data.items():
-                    setattr(recipe, attr, value)
-                db.session.commit()
-                return recipe.to_dict(), 200
-            else:
-                return {"error": "Recipe not found"}, 404
-        except Exception as e:
-            return {"error": "Failed to update recipe", "message": str(e)}, 500
-
-
-api.add_resource(RecipesById, '/recipe/<int:id>')
-
-
-class PopularRecipes(Resource):
-    def get(self):
-        sort_by = request.args.get('_sort')
-        order = request.args.get('_order')
-        limit = request.args.get('_limit')
-
-        valid_sort_fields = ['likes']  # Add more fields if needed
-        if sort_by not in valid_sort_fields:
-            return {"error": "Invalid sort field"}, 400
-
-        if order == 'desc':
-            recipes = Recipe.query.order_by(
-                desc(getattr(Recipe, sort_by))).all()
-        else:
-            recipes = Recipe.query.order_by(getattr(Recipe, sort_by)).all()
-
-        if limit is not None and limit.isdigit():
-            recipes = recipes[:int(limit)]
-
-        return [recipe.to_dict() for recipe in recipes]
-
-
-api.add_resource(PopularRecipes, '/popular_recipes')
-
-
-class NewestRecipes(Resource):
-    def get(self):
-        sort_by = request.args.get('_sort')
-        order = request.args.get('_order')
-        limit = request.args.get('_limit')
-
-        valid_sort_fields = ['id']  # Add more fields if needed
-        if sort_by not in valid_sort_fields:
-            return {"error": "Invalid sort field"}, 400
-
-        if order == 'desc':
-            recipes = Recipe.query.order_by(
-                desc(getattr(Recipe, sort_by))).all()
-        else:
-            recipes = Recipe.query.order_by(getattr(Recipe, sort_by)).all()
-
-        if limit is not None and limit.isdigit():
-            recipes = recipes[:int(limit)]
-
-        return [recipe.to_dict() for recipe in recipes]
-
-
-api.add_resource(NewestRecipes, '/newest_recipes')
-
-# Cocktail Routes
-
-
-class Cocktails(Resource):
-
-    def get(self):
-        try:
-            cocktails = [cocktail.to_dict()
-                         for cocktail in Cocktail.query.all()]
-            return cocktails, 200
-        except Exception as e:
-            return {"error": "Failed to retrieve Cocktail", "message": str(e)}, 500
-
-    def post(self):
-        try:
-            cocktail_data = request.get_json()
-
-            new_cocktail = Cocktail(**cocktail_data)
-
-            db.session.add(new_cocktail)
-
-            db.session.commit()
-
-            return new_cocktail.to_dict(), 201
-
-        except Exception as e:
-            return {"error": "Failed to create cocktail", "message": str(e)}, 500
-
-
-api.add_resource(Cocktails, '/cocktail')
-
-
-class CocktailsById(Resource):
-
-    def get(self, id):
-        try:
-            cocktail = Cocktail.query.filter_by(id=id).first().to_dict()
-            return cocktail, 200
-        except Exception as e:
-            return {"error": "Failed to find cocktail", "message": str(e)}, 500
-
-    def patch(self, id):
-        try:
-            data = request.get_json()
-            cocktail = Cocktail.query.filter_by(id=id).first()
-
-            if cocktail:
-                for attr, value in data.items():
-                    setattr(cocktail, attr, value)
-                db.session.commit()
-                return cocktail.to_dict(), 200
-            else:
-                return {"error": "cocktail not found"}, 404
-        except Exception as e:
-            return {"error": "Failed to update cocktail", "message": str(e)}, 500
-
-
-api.add_resource(CocktailsById, '/cocktail/<int:id>')
-
-
-class PopularCocktails(Resource):
-    def get(self):
-        sort_by = request.args.get('_sort')
-        order = request.args.get('_order')
-        limit = request.args.get('_limit')
-
-        valid_sort_fields = ['likes']  # Add more fields if needed
-        if sort_by not in valid_sort_fields:
-            return {"error": "Invalid sort field"}, 400
-
-        if order == 'desc':
-            cocktails = Cocktail.query.order_by(
-                desc(getattr(Cocktail, sort_by))).all()
-        else:
-            cocktails = Cocktail.query.order_by(
-                getattr(Cocktail, sort_by)).all()
-
-        if limit is not None and limit.isdigit():
-            cocktails = cocktails[:int(limit)]
-
-        return [cocktail.to_dict() for cocktail in cocktails]
-
-
-api.add_resource(PopularCocktails, '/popular_cocktails')
-
-
-class OurPicks(Resource):
-
-    def get(self):
-        ourpicks = [pick.to_dict() for pick in OurPick.query.all()]
-        return ourpicks, 200
-
-
-api.add_resource(OurPicks, '/our_picks')
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+b='likes'
+a='password'
+X=None
+W='desc'
+V='Invalid sort field'
+U='_limit'
+T='_order'
+S='_sort'
+R='id'
+Q=True
+P=int
+O=setattr
+J=getattr
+F='error'
+from flask_migrate import Migrate as c
+from sqlalchemy import desc as N
+from flask import request as A
+from flask_restful import Resource as C
+from flask_login import LoginManager as d,login_user as Y,logout_user as e,login_required as H,current_user as K
+from api.config import app as M,db as B,api as D
+from api.models import db as B,User as L,Recipe as E,Cocktail as G,OurPick as f,Favorite as I
+t=c(M,B)
+Z=d()
+Z.init_app(M)
+@Z.user_loader
+def u(user_id):return L.query.filter_by(id=user_id).first()
+class g(C):
+	def post(E):C=A.get_json();D=L(username=C['username'],name=C['name'],email=C['email']);D.password_hash=C[a];B.session.add(D);B.session.commit();Y(D,remember=Q);return D.to_dict(),201
+class h(C):
+	def post(G):
+		C=A.get_json();D=C.get('identifier');E=C.get(a);B=L.query.filter((L.email==D)|(L.username==D)).first()
+		if B:
+			if B.authenticate(E):Y(B,remember=Q);return B.to_dict(),200
+		if not B:return{F:'404 user not found'},404
+@M.route('/logout',methods=['POST'])
+@H
+def v():e();return f"You have logged out. Goodbye"
+class i(C):
+	def get(B):
+		if K.is_authenticated:A=K.to_dict();return A,200
+		return{F:'unauthorized'},401
+class j(C):
+	@H
+	def get(self):A=K.id;B=I.query.filter_by(user_id=A).all();C=[A.to_dict()for A in B];return C,200
+	@H
+	def post(self):
+		C=A.get_json();D=C.get('type')
+		if D=='recipes':E=I(user_id=K.id,recipe_id=C[R])
+		elif D=='cocktail':E=I(user_id=K.id,cocktail_id=C[R])
+		else:return{F:'Invalid favorite type'},400
+		B.session.add(E);B.session.commit();return'Added to favorites',201
+class k(C):
+	@H
+	def get(self,id):A=I.query.filter_by(id=id).first().to_dict();return A,200
+	@H
+	def patch(self,id):
+		D=A.get_json();C=I.query.filter_by(id=id).first()
+		if C:
+			for(E,G)in D.items():O(C,E,G)
+			B.session.commit();return C.to_dict(),200
+		else:return{F:'favorite not found'},404
+	@H
+	def delete(self,id):
+		A=I.query.filter_by(id=id).first()
+		if A:B.session.delete(A);B.session.commit();return{},204
+		else:return{F:'Favorite not found'},404
+D.add_resource(h,'/logins')
+D.add_resource(g,'/signups')
+D.add_resource(i,'/check_session')
+D.add_resource(j,'/users/favorites')
+D.add_resource(k,'/users/favorites/<int:id>')
+class l(C):
+	def get(B):A=[A.to_dict()for A in E.query.all()];return A,200
+	def post(F):D=A.get_json();C=E(**D);B.session.add(C);B.session.commit();return C.to_dict(),201
+D.add_resource(l,'/recipe')
+class m(C):
+	def get(B,id):A=E.query.filter_by(id=id).first().to_dict();return A,200
+	def patch(I,id):
+		D=A.get_json();C=E.query.filter_by(id=id).first()
+		if C:
+			for(G,H)in D.items():O(C,G,H)
+			B.session.commit();return C.to_dict(),200
+		else:return{F:'Recipe not found'},404
+D.add_resource(m,'/recipe/<int:id>')
+class n(C):
+	def get(I):
+		C=A.args.get(S);G=A.args.get(T);D=A.args.get(U);H=[b]
+		if C not in H:return{F:V},400
+		if G==W:B=E.query.order_by(N(J(E,C))).all()
+		else:B=E.query.order_by(J(E,C)).all()
+		if D is not X and D.isdigit():B=B[:P(D)]
+		return[A.to_dict()for A in B]
+D.add_resource(n,'/popular_recipes')
+class o(C):
+	def get(I):
+		C=A.args.get(S);G=A.args.get(T);D=A.args.get(U);H=[R]
+		if C not in H:return{F:V},400
+		if G==W:B=E.query.order_by(N(J(E,C))).all()
+		else:B=E.query.order_by(J(E,C)).all()
+		if D is not X and D.isdigit():B=B[:P(D)]
+		return[A.to_dict()for A in B]
+D.add_resource(o,'/newest_recipes')
+class p(C):
+	def get(B):A=[A.to_dict()for A in G.query.all()];return A,200
+	def post(E):D=A.get_json();C=G(**D);B.session.add(C);B.session.commit();return C.to_dict(),201
+D.add_resource(p,'/cocktail')
+class q(C):
+	def get(B,id):A=G.query.filter_by(id=id).first().to_dict();return A,200
+	def patch(I,id):
+		D=A.get_json();C=G.query.filter_by(id=id).first()
+		if C:
+			for(E,H)in D.items():O(C,E,H)
+			B.session.commit();return C.to_dict(),200
+		else:return{F:'cocktail not found'},404
+D.add_resource(q,'/cocktail/<int:id>')
+class r(C):
+	def get(I):
+		C=A.args.get(S);E=A.args.get(T);D=A.args.get(U);H=[b]
+		if C not in H:return{F:V},400
+		if E==W:B=G.query.order_by(N(J(G,C))).all()
+		else:B=G.query.order_by(J(G,C)).all()
+		if D is not X and D.isdigit():B=B[:P(D)]
+		return[A.to_dict()for A in B]
+D.add_resource(r,'/popular_cocktails')
+class s(C):
+	def get(B):A=[A.to_dict()for A in f.query.all()];return A,200
+D.add_resource(s,'/our_picks')
+if __name__=='__main__':M.run(port=5555,debug=Q)
